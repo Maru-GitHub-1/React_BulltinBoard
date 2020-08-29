745ec36f3e4b2e4942c9dba244a319d4b0fdd8a8
Maru <r51m12a1251@gmail.com>
2020-08-29 15:35:06 +0900

    Created Header component

---
+++ src/components/Header.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import '../components/Header.css';
+
+class Header extends React.Component {
+
+  render() {
+    return (
+      <header class="header">
+        <h1 class="logo"> Vue BulltenBoard </h1>
+      </header>
+    )
+  }
+}
