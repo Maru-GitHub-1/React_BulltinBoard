59fc34223beb8ce51d3b047a650f3e1a34ecc462
Maru <r51m12a1251@gmail.com>
2020-08-30 17:24:35 +0900

    Created TextBox component

---
+++ src/components/TextBox/TextBox.jsx
@@ -0,0 +1,61 @@
+import React from 'react';
+import Button from '../Button/Button'
+import './TextBox.css';
+
+class TextBox extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      body: '',
+    }
+
+    this.handleChange = this.handleChange.bind(this);
+    this.sendBodyToParent = this.sendBodyToParent.bind(this);
+  }
+
+  handleChange(event) {
+    const field = event.target.name;
+
+    this.setState({
+      [field]: event.target.value
+    })
+  }
+
+  sendBodyToParent() {
+    const body = this.state.body.trim();
+    if (!body) {
+      alert('何も入力されていません');
+      return;
+    }
+
+    if (typeof this.props.onSubmit === 'function') {
+      this.props.onSubmit(body);
+    }
+
+    this.setState({
+      body: ''
+    })
+  }
+
+  render() {
+    return (
+      <div className="textbox-container">
+          <textarea
+            placeholder="ここに入力"
+            className="textbox-input"
+            name="body"
+            cols="30"
+            rows="15"
+            value={this.state.body}
+            onChange={this.handleChange}
+          ></textarea>
+        <div className="textbox-button">
+        <Button onClickHundler={this.sendBodyToParent}>投稿する</Button>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default TextBox;
